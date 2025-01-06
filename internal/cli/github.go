4f81a686dca4a4bb40203d5f5959c5aa1387b319
Ignacio Olave <me@iolave.com>
2025-01-06 17:01:01 -0300

    migrated list user and orgs projects feature to go

--- internal/cli/github.go
+++ internal/cli/github.go
@@ -1,5 +1,14 @@
 package cli
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/iolave/jira-tickets-from-gh/internal/github"
+)
+
 type GithubCmd struct {
 	ListProject *GithubListProjectCmd `arg:"subcommand:list-projects"`
 }
@@ -15,6 +24,11 @@ func GithubProjectListAction(args Cmd) {
 		exitOnInvalidCall("github list-projects")
 	}
 
+	if args.GithubToken == nil {
+		err := errors.New(`please set the "GITHUB_TOKEN" env variable`)
+		exitFromErr(err)
+	}
+
 	if args.Github.ListProject.Org == nil && args.Github.ListProject.User == nil {
 		exitOnMissingFlags("--org", "--user")
 	}
@@ -23,5 +37,28 @@ func GithubProjectListAction(args Cmd) {
 		exitOnConflictingFlags("--org", "--user")
 	}
 
-	// TODO: logic goes here
+	gh := github.New(*args.GithubToken)
+	if args.Github.ListProject.User != nil {
+		result, _, err := gh.ListUserProjects(*args.Github.ListProject.User)
+		if err != nil {
+			exitFromErr(err)
+		}
+		b, err := json.Marshal(result.Data.User.Projects.Nodes)
+		if err != nil {
+			exitFromErr(err)
+		}
+		fmt.Println(string(b))
+		os.Exit(0)
+	} else {
+		result, _, err := gh.ListOrganizationProjects(*args.Github.ListProject.Org)
+		if err != nil {
+			exitFromErr(err)
+		}
+		b, err := json.Marshal(result.Data.Organization.Projects.Nodes)
+		if err != nil {
+			exitFromErr(err)
+		}
+		fmt.Println(string(b))
+		os.Exit(0)
+	}
 }
